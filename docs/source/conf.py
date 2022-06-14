a7c36800b56168b1d3641a799ad55613a4812cc0
Anton Komlev <anton.komlev@arm.com>
2022-06-14 12:17:56 +0100

    change language to En

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,6 +91,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
+# Changed from None to en
 language = 'en'
 
 # List of patterns, relative to source directory, that match files and
